e11f57c7bf4ac671d9324d6d4511afcb4a873bc2
Markus Viola <markus.xamuel.viola@gmail.com>
2018-11-22 00:11:50 +0900

    Started the factories for the registries & parameterizer contracts

--- contracts/Orchestrator.sol
+++ contracts/Orchestrator.sol
@@ -1,25 +1,5 @@
 pragma solidity ^0.4.24;
 
-import "./PLCRFactory.sol";
-import "./ParameterizerFactory.sol";
-import "./RegistryFactory.sol";
-
 contract Orchestrator {
-    
-    PLCRFactory plcrFactory;
-    mapping (address => ParameterizerFactory) private paramsFactories;
-    mapping (address => RegistryFactory) private regFactories;
 
-    //This contract only needs one instance of PLCRFactory
-    constructor() public {
-        plcrFactory = new PLCRFactory();
-    }
-    
-    function buildGoverningEnvironment() public {
-        paramFactories[msg.sender] = new ParameterizerFactory(plcrFactory);
-    }
-    
-    function buildTRCEnvironment() public {
-        regFactories[msg.sender] = new RegistryFactory(paramFactory);
-    }
 }
