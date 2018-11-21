5e82289524d1ccd4e7c7a0c7d61a30ac16ba49f6
Markus Viola <markus.xamuel.viola@gmail.com>
2018-11-22 00:14:44 +0900

    Implemented a main contract 'orchestrator' for the API source.

--- contracts/Orchestrator.sol
+++ contracts/Orchestrator.sol
@@ -1,5 +1,25 @@
 pragma solidity ^0.4.24;
 
-contract Orchestrator {
+import "./PLCRFactory.sol";
+import "./ParameterizerFactory.sol";
+import "./RegistryFactory.sol";
 
+contract Orchestrator {
+    
+    PLCRFactory plcrFactory;
+    mapping (address => ParameterizerFactory) private paramsFactories;
+    mapping (address => RegistryFactory) private regFactories;
+
+    //This contract only needs one instance of PLCRFactory
+    constructor() public {
+        plcrFactory = new PLCRFactory();
+    }
+    
+    function buildGoverningEnvironment() public {
+        paramFactories[msg.sender] = new ParameterizerFactory(plcrFactory);
+    }
+    
+    function buildTRCEnvironment() public {
+        regFactories[msg.sender] = new RegistryFactory(paramFactory);
+    }
 }
