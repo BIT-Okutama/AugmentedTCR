f043dbdc50009417dd9e6ee5b0308d94296ad012
Markus Viola <markus.xamuel.viola@gmail.com>
2018-11-22 16:35:23 +0900

    Created functions for making new parameterizer, with and without tokens.

--- contracts/Parameterizer.sol
+++ contracts/Parameterizer.sol
@@ -1,4 +1,4 @@
-pragma solidity^0.4.24;
+pragma solidity^0.4.11;
 
 import "./PLCRVoting.sol";
 import "./EIP20.sol";
@@ -6,7 +6,39 @@ import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/Saf
 
 contract Parameterizer {
     
-    constructor() public {
-        //IT STARTS HERE
+    
+    mapping(bytes32 => uint) public params;
+    
+    EIP20Interface public token;
+    PLCRVoting public voting;
+    
+    
+    function init(address _token, address _plcr, uint256[] _parameters) public {
+        
+        require(_token != 0 && address(token) == 0);
+        require(_plcr != 0 && address(voting) == 0);
+
+        token = EIP20Interface(_token);
+        voting = PLCRVoting(_plcr);
+        
+        set("minDeposit", _parameters[0]);
+        set("pMinDeposit", _parameters[1]);
+        set("applyStageLen", _parameters[2]);
+        set("pApplyStageLen", _parameters[3]);
+        set("commitStageLen", _parameters[4]);
+        set("pCommitStageLen", _parameters[5]);
+        set("revealStageLen", _parameters[6]);
+        set("pRevealStageLen", _parameters[7]);
+        set("dispensationPct", _parameters[8]);
+        set("pDispensationPct", _parameters[9]);
+        set("voteQuorum", _parameters[10]);
+        set("pVoteQuorum", _parameters[11]);
+        set("exitTimeDelay", _parameters[12]);
+        set("exitPeriodLen", _parameters[13]);
+        
+    }
+    
+    function set(string _name, uint256 _value) public {
+        params[keccak256(abi.encodePacked(_name))] = _value;
     }
 }
--- contracts/ParameterizerFactory.sol
+++ contracts/ParameterizerFactory.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.24;
 
+
 import "./PLCRFactory.sol";
 import "./PLCRVoting.sol";
 import "./Parameterizer.sol";
@@ -9,13 +10,39 @@ contract ParameterizerFactory {
 
     PLCRFactory public plcrFactory;
     ProxyFactory public proxyFactory;
-    Parameterizer public parameterizer;
-
+    Parameterizer public canonParam;
+    
     constructor(PLCRFactory _plcrFactory) public {
         plcrFactory = _plcrFactory;
         proxyFactory = plcrFactory.proxyFactory();
-        parameterizer = new Parameterizer();
+        canonParam = new Parameterizer();
+    }
+    
+    event onCreateParameterizer(address origin, address token, Parameterizer parameterizer, address plcr);
+    
+    function createNewParameterizer(EIP20 _token, uint256[] _parameters) public returns(Parameterizer) {
+        
+        PLCRVoting plcr = plcrFactory.createNewPLCR(_token);
+        
+        Parameterizer parameterizer = Parameterizer(proxyFactory.createProxy(canonParam, ""));
+        parameterizer.init(_token, plcr, _parameters);
+        
+        emit onCreateParameterizer(msg.sender, _token, parameterizer, plcr);
+        return parameterizer;
+    }
+    
+    function createNewParameterizerWithToken(uint256 _supply, string _name, uint8 _decimals, string _symbol, uint256[] _parameters) public returns(Parameterizer) {
+        
+        PLCRVoting plcr = plcrFactory.createNewPLCRWithToken(_supply, _name, _decimals, _symbol);
+        
+        EIP20 token = EIP20(plcr.token());
+        token.transfer(msg.sender, _supply);
+        
+        Parameterizer parameterizer = Parameterizer(proxyFactory.createProxy(canonParam, ""));
+        parameterizer.init(token, plcr, _parameters);
+        
+        emit onCreateParameterizer(msg.sender, token, parameterizer, plcr);
+        return parameterizer;
     }
 
-    // ...
 }
