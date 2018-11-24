67434f6092d9de084abf06f83c0383022ca72a5e
Markus Viola <markus.xamuel.viola@gmail.com>
2018-11-24 14:16:31 +0900

    Initialized contract templates to be reproduced.

--- contracts/AugmentedTCRFactory.sol
+++ contracts/AugmentedTCRFactory.sol
@@ -1,9 +1,23 @@
 pragma solidity ^0.4.24;
 
 
+import "./ProxyFactory.sol";
+import "./PLCRVoting.sol";
+import "./Parameterizer.sol";
+import "./Registry.sol";
 
 contract AugmentedTCRFactory{
     
+    ProxyFactory public proxyFactory;
+    PLCRVoting public canonPLCR;
+    Parameterizer public canonParam;
+    Registry public canonRegistry;
     
+    constructor () public {
+        canonPLCR = new PLCRVoting();
+        proxyFactory = new ProxyFactory();
+        canonParam = new Parameterizer();
+        canonRegistry = new Registry();
+    }
     
 }
