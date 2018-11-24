9cb0c144486060459091dc092972c036b962b51a
Markus Viola <markus.xamuel.viola@gmail.com>
2018-11-24 14:39:27 +0900

    Augmented TCR factory done. Added comments.

--- contracts/AugmentedTCRFactory.sol
+++ contracts/AugmentedTCRFactory.sol
@@ -22,14 +22,18 @@ contract AugmentedTCRFactory{
     
     event onCreateEnvironment(address origin, Parameterizer param, Registry reg, EIP20 token, PLCRVoting plcr);
     
+    //To create new augmented TCR environment with an existing token.
     function createNewEnvironment(EIP20 _token, string _registryName, uint256[] _parameters) public returns(Registry reg, Parameterizer param, PLCRVoting plcr) {
         
+        //Instantiates new Partial Lock Commit/Reveal poll handler.
         plcr = PLCRVoting(proxyFactory.createProxy(canonPLCR, ""));
         plcr.init(_token);
         
+        //Instantiates new Parameterizer for a reg.
         param = Parameterizer(proxyFactory.createProxy(canonParam, ""));
         param.init(_token, plcr, _parameters);
         
+        //Instantiates new Registry for listings.
         reg = Registry(proxyFactory.createProxy(canonRegistry, ""));
         reg.init(_token, _registryName, param, plcr);
         
@@ -38,17 +42,22 @@ contract AugmentedTCRFactory{
         
     }
     
+    //To create new augmented TCR environment with an existing token.
     function createNewEnvironmentWithToken(uint256 _supply, string _tokenName, uint8 _decimals, string _symbol, uint256[] _parameters, string _registryName) public returns(Registry reg, Parameterizer param, PLCRVoting plcr) {
     
+        //Instantiates new token for the environment.
         EIP20 token = new EIP20(_supply, _tokenName, _decimals, _symbol);
         token.transfer(msg.sender, _supply);
     
+        //Instantiates new Partial Lock Commit/Reveal poll handler.
         plcr = PLCRVoting(proxyFactory.createProxy(canonPLCR, ""));
         plcr.init(token);
         
+        //Instantiates new Parameterizer for a reg.
         param = Parameterizer(proxyFactory.createProxy(canonParam, ""));
         param.init(token, plcr, _parameters);
         
+        //Instantiates new Registry for listings.
         reg = Registry(proxyFactory.createProxy(canonRegistry, ""));
         reg.init(token, _registryName, param, plcr);
         
