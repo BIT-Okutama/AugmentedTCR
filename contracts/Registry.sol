dae4847818d3010753ee2a537ed6f2eb56e39800
Markus Viola <markus.xamuel.viola@gmail.com>
2018-11-27 22:03:34 +0900

    Added batch processing function for claiming incentive in registry contract.

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -159,6 +159,10 @@ contract Registry {
         emit IncentiveClaimed(msg.sender, _challengeID, reward);
     }
 
+    function batchClaimIncentives(uint256[] _challengeIDs) public {
+        for(uint256 = 0; i < _challengeIDs.length; i++) claimIncentive(_challengeIDs[i]);
+    }
+
     function viewVoterIncentive(address _voter, uint _challengeID) public view returns(uint256) {
         uint256 voterStake = voting.getNumPassingTokens(_voter, _challengeID);
         uint256 total = challenges[_challengeID].wonTokens;
